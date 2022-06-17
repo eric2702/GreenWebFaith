a725459af82e601201ba1369b5d13c7520512489
Eric <3ric8t@gmail.com>
2022-06-17 21:01:47 +0700

    initial commit

---
+++ connection.php
@@ -0,0 +1,18 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+// Create connection
+$con = mysqli_connect($servername, $username, $password,"projecttekweb");
+
+$con2 = mysqli_connect($servername, $username, $password,"proyekdl");
+
+
+// Check connection
+if (!$con) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+session_start();
+?>
