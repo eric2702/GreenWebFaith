fad869e655c319eb866accf640e146a7c00a7a84
JeremyDionP <jeremydionp@gmail.com>
2022-06-24 22:04:28 +0700

    form setting

---
+++ form.php
@@ -0,0 +1,127 @@
+<?php
+    include_once "addForm.php";
+    $email = $_SESSION['email'];
+    $sql = "SELECT * FROM user_data WHERE email='$email'";
+$result = mysqli_query($con, $sql);
+
+$row = mysqli_fetch_assoc($result);
+$profileImg = $row['profileImg'];
+$bio = $row['bio'];
+?>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Image Preview and Upload PHP</title>
+    <!-- Bootstrap CSS -->
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
+    <link rel="stylesheet" href="main.css">
+    <script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
+    <link rel="icon" href="assets\logo.png" sizes="20">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-sm-2 col-0"></div>
+            <div class="col-12 col-sm-8 col-lg-4 form-div">
+                <a style="text-decoration: none;" href="myprofile.php"><i class="fas fa-chevron-left"></i>Back</a>
+                <form action="form.php" method="post" enctype="multipart/form-data">
+                    <h2 class="text-center mb-3 mt-3">Update profile</h2>
+                    <?php if (!empty($msg)): ?>
+                    <div class="alert <?php echo $msg_class ?>" role="alert">
+                        <?php echo $msg; ?>
+                    </div>
+                    <?php endif; ?>
+                    <div class="form-group text-center" style="position: relative;">
+                        <span class="img-div">
+                            <div class="text-center img-placeholder" onClick="triggerClick()">
+                                <h4>Update image</h4>
+                            </div>
+                            <img src="<?php echo $profileImg ?>" onClick="triggerClick()" id="profileDisplay">
+                        </span>
+                        <input type="file" name="profileImage" accept="image/png, image/gif, image/jpeg" onChange="displayImage(this)" id="profileImage"
+                            class="form-control" style="display: none;">
+                        <label>Profile Image</label>
+                    </div>
+                    <div class="form-group">
+                        <label>Bio</label>
+                        <textarea id="isibio" name="bio" class="form-control"></textarea>
+                    </div>
+                    <div class="form-group mt-3">
+                        <button type="submit" name="save_profile" class="btn btn-primary btn-block">Save User</button>
+                    </div>
+                    <button type="button" data-bs-toggle="modal" data-bs-target="#myModal" name="upgrade" class="btn btn-primary mt-3">Be A Designer</button>
+                </form>
+            </div>
+            <div class="col-lg-4 col-sm-2 col-0"></div>
+        </div>
+    </div>
+
+
+<div class="modal" id="myModal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+
+      <!-- Modal Header -->
+      <div class="modal-header">
+        <h4 class="modal-title">Request Form</h4>
+        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+      </div>
+
+      <!-- Modal body -->
+      <div class="modal-body">
+        <div class="form-group">
+          <label>Phone Number</label>
+          <textarea id="telpon" name="nohp" class="form-control" placeholder="Phone Numbe..."></textarea>
+          <label>Address</label>
+          <textarea id="address" name="alamat" class="form-control" placeholder="Address..."></textarea>
+          <label>Bank Name</label>
+          <textarea id="bankname" name="namabank" class="form-control" placeholder="Bank Name..."></textarea>
+          <label>Bank Account</label>
+          <textarea id="bankacc" name="rekening" class="form-control" placeholder="Bank Account..."></textarea>
+          <div class="form-group mt-3">
+                <button type="submit" name="req_dsgn" class="btn btn-primary btn-block">Submit</button>
+          </div>
+        </div>
+                    
+      </div>
+
+    </div>
+  </div>
+</div>
+</body>
+
+</html>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+</script>
+<script>
+$(document).ready(function() {
+    var bio = '<?php echo $bio ?>';
+
+    $("#isibio").val(bio);
+
+});
+
+
+function triggerClick(e) {
+    document.querySelector('#profileImage').click();
+}
+
+function displayImage(e) {
+    if (e.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            document.querySelector('#profileDisplay').setAttribute('src', e.target.result);
+        }
+        reader.readAsDataURL(e.files[0]);
+    }
+}
+</script>
